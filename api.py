7fdc3be4ca1d3481cbd4a1c3b1d5268d49a0714f
rmatos42 <ryleymatos@gmail.com>
2017-08-09 08:39:05 -0700

    cross-origin works now

--- api.py
+++ api.py
@@ -6,11 +6,60 @@ app = Flask(__name__)
 client = MongoClient('mongodb://artikuser:iot2017@45.55.4.31/workshop')
 db = client.workshop
 
+from flask import Flask, make_response, request, current_app
+from datetime import timedelta
+from functools import update_wrapper
+
+app = Flask(__name__)
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 @app.route('/collections', methods=['GET'])
+@crossdomain(origin="*")
 def get_collections():
 	return jsonify({'collections': db.collection_names()})
 
 @app.route('/collections/<string:collection_name>', methods=['GET'])
+@crossdomain(origin="*")
 def get_collection(collection_name):
 	collection = db[collection_name]
 	output = []
@@ -20,6 +69,7 @@ def get_collection(collection_name):
 	return jsonify({collection_name: output})
 
 @app.route('/collections/<string:collection_name>/<string:entry_id>', methods=['GET'])
+@crossdomain(origin="*")
 def get_entry(collection_name, entry_id):
 	collection = db[collection_name]
 	output = []
@@ -29,6 +79,7 @@ def get_entry(collection_name, entry_id):
 	return jsonify(output[0])
 
 @app.errorhandler(404)
+@crossdomain(origin="*")
 def not_found(error):
 	return make_response(jsonify({'error': 'Not found'}), 404)
 
