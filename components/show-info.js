6ae8dbcbc57138005c8bb78e2e9d823fd11448dd
rmatos42 <ryleymatos@gmail.com>
2017-08-14 21:43:46 +0000

    added polling

--- components/show-info.js
+++ components/show-info.js
@@ -2,7 +2,9 @@ AFRAME.registerComponent('show-info', {
 	schema: {default: ''},
 	init() {
 		this.el.addEventListener('click', () => {
-			request_end(this.el.id);
+			g_endpoint = this.el.id;
+			console.log(this.el.id);
+			//g_update = 1;
 		});
 	}
 });
