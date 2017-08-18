42fba7fe39211fac10f2eaeef129e625340cc213
rmatos42 <ryleymatos@gmail.com>
2017-08-18 07:20:43 +0000

    all clean

--- api.py
+++ api.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request, current_app, render_template
+from flask_sockets import Sockets
 from pymongo import MongoClient
-
-app = Flask(__name__)
-client = MongoClient('mongodb://artikuser:iot2017@45.55.4.31/workshop')
-db = client.workshop
-
-from flask import Flask, make_response, request, current_app
 from datetime import timedelta
 from functools import update_wrapper
+import time
 
 app = Flask(__name__)
+sockets = Sockets(app)
+client = MongoClient('mongodb://artikuser:iot2017@45.55.4.31/workshop')
+db = client.workshop
+latest = ""
 
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
@@ -53,6 +52,11 @@ def crossdomain(origin=None, methods=None, headers=None,
         return update_wrapper(wrapped_function, f)
     return decorator
 
+@app.route('/', methods=['GET'])
+@crossdomain(origin="*")
+def serve_webage():
+	return render_template('index.html')
+
 @app.route('/collections', methods=['GET'])
 @crossdomain(origin="*")
 def get_collections():
@@ -78,8 +82,26 @@ def get_field(collection_name, entry_id, field_id):
 	print(output)
 	return jsonify({'_id': entry_id, field_id: output[0]['payload'][field_id]})
 
+@app.route('/collections/<string:collection_name>/<string:entry_id>/change', methods=['GET'])
+@crossdomain(origin="*")
+def check_new(collection_name, entry_id):
+	global latest;
+	collection = db[collection_name]
+	output = []
+	for d in collection.find({'_id': entry_id}):
+		output.append(d)
+	if latest != output[0]['_msgid'] and latest != "":
+		latest = output[0]['_msgid']
+		print("changed")
+		return jsonify(True)
+	elif latest == "":
+		latest = output[0]['_msgid']
+	print("not changed")
+	return jsonify(False)
+
 @app.route('/collections/<string:collection_name>/<string:entry_id>', methods=['GET'])
 @crossdomain(origin="*")
+#@app.run(threaded=True)
 def get_entry(collection_name, entry_id):
 	collection = db[collection_name]
 	output = []
@@ -88,11 +110,21 @@ def get_entry(collection_name, entry_id):
 	print(output)
 	return jsonify(output[0])
 
+def get_item(collection_name, entry_id):
+	global latest;
+	collection = db[collection_name]
+	output = []
+	for d in collection.find({'_id': entry_id}):
+		output.append(d)
+	return output[0]
 
-@app.errorhandler(404)
-@crossdomain(origin="*")
-def not_found(error):
-	return make_response(jsonify({'error': 'Not found'}), 404)
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+	from gevent import pywsgi
+	from geventwebsocket.handler import WebSocketHandler
+ 	#last_ts = db.oplog.rs.find().sort('$natural', -1)[0]['ts'];
+	#print(last_ts);
+	#app.run(threaded=True)
+	server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+	server.serve_forever()
